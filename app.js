5014903794d03fcafc70a90f5232d101cd5400a7
natsumi <mij.natsumi@gmail.com>
2020-03-22 14:17:08 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 'use strict';
 
+//配列を定義
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
 function fib(n) {
+  if(memo.has(n)) {
+   //配列に値がある場合
+    return memo.get(n);
+  }
+   //配列に値がない場合
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
+
+
+/* function fib(n) {
   
     if (n === 0) {
     // 0の時は0    
@@ -12,9 +34,9 @@ function fib(n) {
   // 0と1以外の時
   return fib(n - 1) + fib(n - 2);
 }
-
-const length = 40;
+ */
+/* const length = 40;
 
 for (let i = 0; i <= length; i++){
     console.log(fib(i));
-}
+} */
