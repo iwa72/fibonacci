cfa2938232abaaa49fcf53d1a6af385e789336a5
natsumi <mij.natsumi@gmail.com>
2020-03-22 14:03:04 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function fib(n) {
+  
+    if (n === 0) {
+    // 0の時は0    
+    return 0;
+  }else if (n === 1) {
+    // 1の時は1  
+    return 1;
+  }
+  // 0と1以外の時
+  return fib(n - 1) + fib(n - 2);
+}
+
+const length = 40;
+
+for (let i = 0; i <= length; i++){
+    console.log(fib(i));
+}
